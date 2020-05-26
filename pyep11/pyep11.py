b3416d807e3dcc9056a89459c05f510c53efe5bd
Matthew Cousens <mcousen@us.ibm.com>
2020-05-26 11:42:21 -0400

    License updates

--- pyep11/pyep11.py
+++ pyep11/pyep11.py
@@ -1,4 +1,19 @@
 #!/usr/bin/python3
+##############################################################################
+# Copyright 2020 IBM Corp. All Rights Reserved.
+#
+#  Licensed under the Apache License, Version 2.0 (the "License");
+#  you may not use this file except in compliance with the License.
+#  You may obtain a copy of the License at
+#
+#       http://www.apache.org/licenses/LICENSE-2.0
+#
+#   Unless required by applicable law or agreed to in writing, software
+#   distributed under the License is distributed on an "AS IS" BASIS,
+#   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#   See the License for the specific language governing permissions and
+#   limitations under the License.
+##############################################################################
 
 import os, sys, json, grpc, sqlite3, binascii, time
 import grep11consts as ep11, grep11_pb2, pkcs11_pb2, server_pb2, server_pb2_grpc, grep11_pb2_grpc 
